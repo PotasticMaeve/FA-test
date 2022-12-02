00a48b56da215ee2e8941eef05bd8d7780724c87
Claudia Felicia <claudia.ak47@gmail.com>
2022-12-02 22:45:40 +0700

    styling card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,64 @@
-import { Avatar, Card, Col } from 'antd'
-import Meta from 'antd/es/card/Meta'
+import { Badge, Button, Card, Col, Typography } from 'antd'
+import { useState } from 'react';
 import Layout from '../src/components/layout'
 import PROJECT_LIST from '../src/constants/project-list'
 
 export default function Project() {
+  const [expand, setExpand] = useState(false)
+  const [isEllipsis, setIsEllipsis] = useState(true)
+  const [indexOpen, setIndexOpen] = useState(0)
+
   return (
     <Layout seo={{ title: 'Projects' }}>
       <Col className="panel-container">
         <div className="panel">
-          <h3 className="panel-title">Tasks to Do</h3>
+          <h3 className="panel-title">Category A</h3>
           <ul className="panel-items">
             {PROJECT_LIST.map((el, i) => (
               <li key={i}>
                 <Card
-                  style={{ width: '300px' }}
                   cover={
                     <img
-                      alt="example"
+                      alt={el.title}
                       src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
                     />
                   }
                 >
-                  <Meta
-                    avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                    title={el.title}
-                    description={el.project_overview}
-                  />
+                  <Badge count={el.carbon} color='#E97777' />
+                  <Typography.Text className="card-title">{el.title}</Typography.Text>
+                  <Col span={24}>
+                    {expand ? (
+                      <Typography.Paragraph
+                        className='card-desc'
+                      >
+                        {el.project_overview}
+                      </Typography.Paragraph>
+                    ) : (
+                      <Typography.Paragraph
+                        ellipsis={{
+                          rows: expand ? undefined : 3,
+                          expandable: expand,
+                          symbol: <></>,
+                          onEllipsis: (ell) => setIsEllipsis(ell)
+                        }}
+                        className='card-desc'
+                      >
+                        {el.project_overview}
+                      </Typography.Paragraph>
+                    )}
+                  </Col>
+                  {isEllipsis ? (
+                    <Button
+                      type='text'
+                      className='card-btn'
+                      size='small'
+                      onClick={() => setExpand(!expand)}
+                    >
+                      {expand ? 'Hide' : 'Read More'}
+                    </Button>
+                  ) : (
+                    <></>
+                  )}
                 </Card>
               </li>
             ))}
--- src/constants/project-list.ts
+++ src/constants/project-list.ts
@@ -9,7 +9,7 @@ const PROJECT_LIST = [
         certification: 'Plan Vivo PM001',
         project_overview: 'This project is a community agroforestry project that aims to increase carbon sequestration in the area and improve the livelihoods of the project participants through additional incomes from carbon credit and yield production as well as improving ecosystem quality. Implemented on 75 hectares of land by Fairatmos, Edufarmers, and Forestry Office of West Java.',
         impact_highlight: 'Increasing carbon emissions sequestration, improving local economy through additional incomes from carbon credit and fruit yield, reducing the degradation of surrounding ecosystems.',
-        carbon: 100
+        carbon: 10
     },
     {
         id: 1,
@@ -19,9 +19,9 @@ const PROJECT_LIST = [
         location: 'Ujungalang village, Cilacap Regency, Central Java, Indonesia',
         started: '2023',
         certification: 'VCS ARM0014',
-        project_overview: 'he project is a mangrove restoration project with the objective of creating a healthy mangrove ecosystem on a 2000 hectares land which will be conducted by Fairatmos in collaboration with Lindungi Hutan.',
+        project_overview: 'The project is a mangrove restoration project with the objective of creating a healthy mangrove ecosystem on a 2000 hectares land which will be conducted by Fairatmos in collaboration with Lindungi Hutan.',
         impact_highlight: 'Increasing carbon emissions sequestration, improving local economy through local business initiatives of mangrove products, reducing the degradation of surrounding ecosystems.',
-        carbon: 200
+        carbon: 20
     },
     {
         id: 2,
@@ -33,7 +33,7 @@ const PROJECT_LIST = [
         certification: 'VCS ARM0014',
         project_overview: 'The project is a mangrove restoration project with the objective of creating a healthy mangrove ecosystem on a 1530 hectares land which will be conducted by Fairatmos in collaboration with Titian Lestari Foundation.',
         impact_highlight: 'Increasing carbon emissions sequestration, improving local economy through local silvofishery, reducing the degradation of surrounding ecosystems.',
-        carbon: 300
+        carbon: 30
     },
 ]
 
