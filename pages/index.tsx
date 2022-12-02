f6ba52631a1fbe1186e9453950426b88de9ba427
Claudia Felicia <claudia.ak47@gmail.com>
2022-12-03 01:03:45 +0700

    complete search func

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { Button, Card, Col, Typography } from 'antd'
-import { useEffect, useState } from 'react';
+import { Button, Card, Col, Typography, Input } from 'antd'
+import { useState } from 'react';
 import Layout from '../src/components/layout'
 import PROJECT_LIST from '../src/constants/project-list'
 import _ from 'lodash'
@@ -10,6 +10,21 @@ export default function Project() {
   const [isEllipsis, setIsEllipsis] = useState(true)
   const [index, setIndex] = useState(undefined)
   const [isReverseSort, setIsReverseSort] = useState(false)
+  const [showSearch, setShowSearch] = useState(false)
+  const [content, setContent] = useState(PROJECT_LIST)
+
+  const handleChangeSearch = (key) => {
+    if(key !== ""){
+      const data = PROJECT_LIST.filter((el) => {
+        if(el.title.toLowerCase().includes(key.toLowerCase())){
+          return el
+        }
+      })
+      setContent(data)
+    }else{
+      setContent(PROJECT_LIST)
+    }
+  }
 
   return (
     <Layout seo={{ title: 'Projects' }}>
@@ -17,17 +32,37 @@ export default function Project() {
         <div className="panel">
           <div className="panel-header">
             <h3 className="panel-title">Category A</h3>
-            <Button
-              type='text'
-              className='sort-btn'
-              size='small'
-              onClick={() => setIsReverseSort(!isReverseSort)}
-            >
-              {isReverseSort ? <i className="ri-sort-asc" /> : <i className="ri-sort-desc" />}
-            </Button>
+            <div>
+              <Button
+                type='text'
+                className='sort-btn'
+                size='small'
+                onClick={() => setShowSearch(!showSearch)}
+              >
+                {showSearch ? <i className="ri-close-fill" /> : <i className="ri-search-line" />}
+              </Button>
+              <Button
+                type='text'
+                className='sort-btn'
+                size='small'
+                onClick={() => setIsReverseSort(!isReverseSort)}
+              >
+                {isReverseSort ? <i className="ri-sort-asc" /> : <i className="ri-sort-desc" />}
+              </Button>
+            </div>
           </div>
+          {showSearch && (
+            <div>
+              <Input
+                className='search'
+                onChange={(e) => handleChangeSearch(e.target.value)}
+                placeholder='Type project name..'
+                autoFocus
+              />
+            </div>
+          )}
           <ul className="panel-items">
-            {(_.orderBy(PROJECT_LIST, ['carbon'], [isReverseSort ? 'desc' : 'asc'])).map((el, i) => (
+            {(_.orderBy(content, ['carbon'], [isReverseSort ? 'desc' : 'asc'])).map((el, i) => (
               <li key={i}>
                 <Col className='badge-carbon'>
                   <i className="ri-virus-fill" />
